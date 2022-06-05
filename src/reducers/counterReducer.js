1b33d51359a1969843bbc5906ecdcf50bf14d9cb
surbhi322 <81092552+surbhi322@users.noreply.github.com>
2022-06-05 11:33:57 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,10 +1,10 @@
-const initialState = 0;
+const counter = { count: 0 }
 const counterReducer = (state,action) =>{
    switch (action) {
-    case "add":
-      return state + 1;
-    case "subtract":
-      return state - 1;
+    case "increment":
+      return { count: state.count + 1 }
+    case "decrement":
+      return { count: state.count !== 0 ? state.count - 1 : (state.count = 0) }
 }
 
 export {counterReducer}
