a8173ab9a2164d05622983b41182b18d44ddb30b
Surbhi Lande <81092552+surbhi322@users.noreply.github.com>
2022-06-14 23:07:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,18 @@
-import React, { Component, useState, useEffect } from "react";
-import "../styles/App.css";
-
-const App = () => {
-  // write your code here
-  const [seconds, setSeconds] = useState(0);
-
-  useEffect(() => {
-    if (seconds > 0) {
-      var id = setTimeout(() => {
-        setSeconds(seconds - 1);
-        // console.log("Timer active");
-      }, 1000);
-    }
-    return () => {
-      clearTimeout(id);
-    };
-  }, [seconds]);
-
+import react, {useReducer} from 'react';
+import {counterReducer} from '../reducers/CounterReducer.js';
+import '../styles/App.css';
+const App = () =>{
+  const [state, dispatch] = useReducer(counterReducer, 0);
   return (
-    <div className="wrapper">
-      <div id="whole-center">
-        <h1>
-          Reverse countdown for
-          <input
-            id="timeCount"
-            onKeyDown={(event) => {
-              let inpVal = event.target.value;
-              if (event.keyCode === 13) {
-                if (isNaN(inpVal) || inpVal < 0) {
-                  setSeconds(0);
-                } else {
-                  setSeconds(Math.floor(inpVal));
-                }
-              }
-            }}
-          />{" "}
-          sec.
-        </h1>
-      </div>
-      <div id="current-time">{seconds}</div>
+    <div id = "main">
+    <span id = "counter">{state}</span>
+    <button id="increment-btn" onClick={function(){
+      dispatch('INC');
+    }}>INC</button>
+    <button id = "decrement-btn" onClick={function(){
+      dispatch('DEC');
+    }}>DEC</button>
     </div>
-  );
-};
-
+  )
+}
 export default App;
