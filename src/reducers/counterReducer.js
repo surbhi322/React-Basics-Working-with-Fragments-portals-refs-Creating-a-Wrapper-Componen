de96ec13aa82cb6d1cc27b7a0ee219ac66f2ddae
Surbhi Lande <81092552+surbhi322@users.noreply.github.com>
2022-06-14 22:57:47 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,11 +1,9 @@
-const counter = { count: 0 }
 const counterReducer = (state,action) =>{
-   switch (action) {
-    case "increment":
-      return { count: state.count + 1 }
-    case "decrement":
-      return { count: state.count !== 0 ? state.count - 1 : (state.count = 0) }
-}
+  if(action === 'INC'){
+     return state + 1;
+  }else if(action ==='DEC'){
+     return state-1;
+  }
 }
 
 export {counterReducer}
